d1e398b8cf2d2e71ab86ec6305c7a0c046c91517
githubfoam <lequestionis@gmail.com>
2020-07-09 17:05:52 +0300

    golang

---
+++ app/deploy-openfaas-golang.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
+
+echo "============================OpenFaaS =============================================================="
+# https://brew.sh/
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+
+brew install go faas-cli kubernetes-cli kubernetes-helm
+
+# Deploy OpenFaaS to minikube
+# Create a service account for Helm’s server component (tiller):
+kubectl -n kube-system create sa tiller && kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+
+# Install tiller which is Helm’s server-side component:
+helm init --skip-refresh --upgrade --service-account tiller
+
+# Create namespaces for OpenFaaS core components and OpenFaaS Functions:
+kubectl apply -f https://raw.githubusercontent.com/openfaas/faas-netes/master/namespaces.ym
+
+# Add the OpenFaaS helm repository:
+helm repo add openfaas https://openfaas.github.io/faas-netes/
+
+# Update all the charts for helm:
+helm repo update
+
+# Create a password (Remember this for login to OpenFaaS dashboard):
+# export PASSWORD=testpassword
+
+# Random password example:
+export PASSWORD=$(head -c 12 /dev/urandom | shasum| cut -d' ' -f1)
+
+# Create a secret for the password:
+kubectl -n openfaas create secret generic basic-auth --from-literal=basic-auth-user=admin --from-literal=basic-auth-password="$PASSWORD"
+
+# Install OpenFaaS using the chart:
+helm upgrade openfaas --install openfaas/openfaas --namespace openfaas --set functionNamespace=openfaas-fn --set basic_auth=true
+
+# Finally once all the Pods are started you can login using the faas-cli:
+echo -n $PASSWORD | faas-cli login -g http://$(minikube ip):31112 -u admin --password-stdin
+
+ # find dashboard URL and open it by browser (username: admin)
+ echo http://$(minikube ip):31112
