de20401d5f99e43864499a4765a1c6cb5d2a5f0b
githubfoam <lequestionis@gmail.com>
2020-07-11 21:42:59 +0300

    typos

--- kind/deploy-kind.sh
+++ kind/deploy-kind.sh
@@ -3,12 +3,9 @@ echo "=============================deploy kind==================================
 docker version
 export KIND_VERSION="0.8.1"
 
-# curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$KIND_VERSION/kind-$(uname)-amd64
-# chmod +x ./kind
-# mv ./kind /usr/local/bin/kind
-curl -Lo kind https://kind.sigs.k8s.io/dl/v$KIND_VERSION/kind-$(uname)-amd64
-chmod +x kind
-mv kind /usr/local/bin/kind
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v$KIND_VERSION/kind-$(uname)-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
 
 kind get clusters #see the list of kind clusters
 kind get clusters
